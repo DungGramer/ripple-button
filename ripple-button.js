0496acc8b999d412f6c1d070fca8349b537fa433
Nguyễn Công Dũng <dung.dev.gramer@gmail.com>
2021-02-26 11:32:57 +0700

    add event for mobile device

--- ripple-button.js
+++ ripple-button.js
@@ -1,26 +1,52 @@
 document.head.innerHTML +=
-	'<link rel="stylesheet" href="https://dunggramer.github.io/ripple-button/ripple-button.css" />';
+  '<link rel="stylesheet" href="https://dunggramer.github.io/ripple-button/ripple-button.css" />';
 
-function createRipple(event) {
-	const button = event.currentTarget;
-	const circle = document.createElement("span");
-	const diameter = Math.max(button.clientWidth, button.clientHeight);
-	const radius = diameter / 2;
-
-	circle.style.width = circle.style.height = `${diameter}px`;
-	circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
-	circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
-	circle.setAttribute("ripple", "");
-
-	button.appendChild(circle);
-
-	setTimeout(() => {
-		button.removeChild(circle);
-	}, 600);
+/**
+ * Check mobile device
+ * return true if is mobile
+ *
+ * @return {boolean}
+ */
+function isMobile() {
+  const regex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
 }
 
-const buttons = document.querySelectorAll("button[ripple]");
+function createRipple(event) {
+  const button = event.currentTarget;
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  const circle = document.createElement('span');
+  circle.setAttribute('ripple', '');
+  circle.style.width = circle.style.height = `${diameter}px`;
+
+  let locationTouch = {
+    x: event.clientX,
+    y: event.clientY,
+  };
+  const locationButton = {
+    x: button.offsetLeft,
+    y: button.offsetTop,
+  };
+
+  if (isMobile()) {
+    locationTouch.x = event.touches[0].clientX;
+    locationTouch.y = event.touches[0].clientY;
+  }
+  circle.style.left = `${locationTouch.x - locationButton.x - radius}px`;
+  circle.style.top = `${locationTouch.y - locationButton.y - radius}px`;
+
+  button.appendChild(circle);
+
+  setTimeout(() => {
+    button.removeChild(circle);
+  }, 700); //timeout = animation-time
+}
+
+const buttons = document.querySelectorAll('button[ripple]');
 
 for (const button of buttons) {
-	button.addEventListener("click", createRipple);
+  const touchEvent = isMobile() ? 'touchstart' : 'mousedown';
+  button.addEventListener(touchEvent, createRipple);
 }
